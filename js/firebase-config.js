2d21101c381c8d6b6294a81d26a1d3ce801b8217
ssshiponu <sshiponuddin22@gmail.com>
2025-05-05 01:08:50 +0600

    files seperated

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBXQRBhL8x3UgQoKmg3K4DGBQGjB5JxOqk",
+    authDomain: "finance-tracker-app-9a5e2.firebaseapp.com",
+    projectId: "finance-tracker-app-9a5e2",
+    storageBucket: "finance-tracker-app-9a5e2.appspot.com",
+    messagingSenderId: "328619421973",
+    appId: "1:328619421973:web:3bdf6ccad7f1a26ec93b3c"
+};
